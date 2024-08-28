a13e56605627cdbcf201b65290248339ec2910ab
Johanna Prinz <Johannaa.L@hotmail.com>
2024-08-28 14:54:37 +0200

    ✨ Added Loader

---
+++ src/loaders/animalsLoader.ts
@@ -0,0 +1,25 @@
+import { IAnimal } from "../models/IAnimal";
+import { getAnimalAPI } from "../service/AnimalService";
+
+export interface IAnimalsLoader {
+  animalList: IAnimal[];
+  error: string;
+}
+
+export const animalsLoader = async (): Promise<IAnimalsLoader> => {
+  const data: IAnimalsLoader = { animalList: [], error: '' };
+
+  try {
+    const response = await getAnimalAPI();
+    if (response) {
+      data.animalList = response;
+    } else {
+      data.error = 'No response from the API';
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+    data.error = 'Something went wrong. Please try again later.';
+  }
+
+  return data;
+};
