6f3b3a746420eeedacb0fa98b1df3c003b39c821
Johanna Prinz <Johannaa.L@hotmail.com>
2024-08-30 09:36:29 +0200

    🚀  Removed base from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/the-zoo-JohannaPri/', 
+  plugins: [react()], 
 })
