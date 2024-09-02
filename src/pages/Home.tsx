580d1b490088e9c9e1b26f8b4066ddf70bf92bc0
Johanna Prinz <Johannaa.L@hotmail.com>
2024-09-02 13:17:43 +0200

    ✨ Added list on home page if animals are hungry

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,94 @@
 import { Link } from "react-router-dom";
 import "../style/home.scss";
 import logo from "/logo-small.png";
+import { useEffect, useState } from "react";
+import { IAnimal } from "../models/IAnimal";
+import { calculateHoursSinceFed } from "../functions/dateTimeUtils";
+import { getAnimalAPI } from "../service/AnimalService"; 
+import { Loader } from "../components/Loader";
 
 export const Home = () => {
+  const [hungryAnimals, setHungryAnimals] = useState<IAnimal[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchAnimals = async () => {
+      try {
+        const storedAnimals = sessionStorage.getItem("animals");
+        let animals: IAnimal[] = [];
+
+        if (storedAnimals) {
+          animals = JSON.parse(storedAnimals);
+        } else {
+          const response = await getAnimalAPI(); 
+          if (response) {
+            sessionStorage.setItem("animals", JSON.stringify(response));
+            animals = response;
+          }
+        }
+
+        const hungry = animals.filter(animal => {
+          const hoursSinceFed = calculateHoursSinceFed(new Date(animal.lastFed));
+          return hoursSinceFed >= 4; 
+        });
+
+        setHungryAnimals(hungry);
+      } catch (error) {
+        console.error("Error fetching animals:", error);
+      } finally {
+        setIsLoading(false); 
+      }
+    };
+
+    fetchAnimals();
+  }, []);
+
+  return (
+    <>
+      {isLoading ? ( 
+        <Loader />
+      ) : (
+        <div className="hero-img">
+          <div className="hero-info">
+            <div className="logo-container">
+              <img src={logo} alt="the zoo logo" width={375} />
+            </div>
+            <Link to="/animals">
+              <button>Våra Djur</button>
+            </Link>
+            <div className="hungry-animals">
+              <h2>Hungriga djur!</h2>
+              {hungryAnimals.length > 0 ? (
+                <ul>
+                  {hungryAnimals.map((animal) => (
+                    <li key={animal.id}>{animal.name}</li>
+                  ))}
+                </ul>
+              ) : (
+                <p>Just nu är inga djur hungriga.</p>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*export const Home = () => {
   return (
     <>
       <div className="hero-img">
@@ -18,3 +104,4 @@ export const Home = () => {
     </>
   );
 };
+*/
