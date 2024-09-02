47c691aebdcbcb463906935961a21dbd702470e6
Johanna Prinz <Johannaa.L@hotmail.com>
2024-09-02 13:22:25 +0200

    📝 Added comments

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -74,34 +74,3 @@ export const Home = () => {
     </>
   );
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*export const Home = () => {
-  return (
-    <>
-      <div className="hero-img">
-        <div className="hero-info">
-          <div className="logo-container">
-           <img src={logo} alt="the zoo logo" width={375} />
-          </div>
-          <Link to="/animals">
-            <button>Våra Djur</button>
-          </Link>
-        </div>
-      </div>
-    </>
-  );
-};
-*/
